cde6fb249d112db4e305006839d41b35ed5f9757
nikhiltambre <nikhiltambre008@gmail.com>
2025-09-10 12:00:24 +0530

    Initial commit

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,24 @@
+-- Simple replacement for your gitsigns.lua file
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      signs_staged = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      signcolumn = true, -- This is the key fix
+      auto_attach = true, -- This ensures it attaches automatically
+    }
+  end,
+}
